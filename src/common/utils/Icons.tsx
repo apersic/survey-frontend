4af073a4d446fea7ee262dd3e059ce66f772bfbf
apersic <alen.persic@metaswitch.com>
2023-02-24 17:05:24 +0100

    Add assets

---
+++ src/common/utils/Icons.tsx
@@ -0,0 +1,7 @@
+import { ReactComponent as Info } from "../../../assets/icons/info.svg";
+import { ReactComponent as Error } from "../../../assets/icons/error.svg";
+
+export {
+    Info,
+    Error,
+}
